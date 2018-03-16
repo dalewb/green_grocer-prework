c0318a150e444602e61b9f228da85c4a2f101348
William Dale <williambdale@gmail.com>
2018-03-16 13:56:09 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  count = Hash.new(0)
+  new_cart = {}
+  cart.each do |item|
+    count[item.keys[0]] += 1
+  end 
+  cart.each do |item|
+    new_cart[item.keys[0]] = item.values[0]
+    new_cart[item.keys[0]][:count] = count[item.keys[0]]
+  end 
+  new_cart 
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {} 
+  cart.each do |item, info|
+    coupons.each do |data|
+      info.keys.each do |key|
+        if data[:item] == item 
+          temp = {} 
+          
+          new_cart["#{item} W/COUPON"]
+        end 
+        binding.pry 
+      end 
+    end 
+  end 
 end
 
 def apply_clearance(cart)
